0e37e2c2d78e9aceae7dbc66f19dce02e7642a68
Anthony Crawford <crawfishmedia@gmail.com>
2023-10-01 23:41:23 -0300

    4.2 upgrade

--- src/sgc/urls.py
+++ src/sgc/urls.py
@@ -1,7 +1,8 @@
-"""sgc URL Configuration
+"""
+URL configuration for sgc project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,17 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include,path
 from django.conf import settings
-#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-#
 
 urlpatterns = [
-	path('', include('media.urls')),
+    path('', include('media.urls')),
     path('admin/', admin.site.urls),
 ]
-
 
 # Development
 if settings.DEBUG:
@@ -35,4 +32,3 @@ if settings.DEBUG:
 # else:
 #     # Production
 #     urlpatterns += staticfiles_urlpatterns()
-
