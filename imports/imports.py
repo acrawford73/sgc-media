abf5b5e2e94cc2ece949844dfdee1aa7030c8abb
Anthony Crawford <crawfishmedia@gmail.com>
2023-10-08 20:22:16 -0300

    .

--- imports/imports.py
+++ imports/imports.py
@@ -2,13 +2,78 @@
 
 import os
 import json
-import psycopg2
+import psycopg
 
-
-def import_countries(data,host):
+# Dictionary with three items
+def import_dict_3(sqlstr, key1, key2, key3, data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
+		cur = conn.cursor()
+		for item in data:
+			#media_mediacountry(country_name,country_code)
+			sql = "INSERT INTO " + sqlstr + " VALUES (%s, %s, %s)"
+			data1 = item[key1]
+			data2 = item[key2]
+			data3 = item[key3]
+			data = (data1,data2,data3,)
+			cur.execute(sql, data)
+			print(str(data1) + ", " + str(data2) + ", " + str(data3))
+	except (Exception, psycopg.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.commit()
+			conn.close()
+
+# Dictionary with two items
+def import_dict_2(sqlstr, key1, key2, data, db_meta):
+	conn = None
+	try:
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
+		cur = conn.cursor()
+		for item in data:
+			#media_mediacountry(country_name,country_code)
+			sql = "INSERT INTO " + sqlstr + " VALUES (%s, %s)"
+			data1 = item[key1]
+			data2 = item[key2]
+			data = (data1,data2,)
+			cur.execute(sql, data)
+			print(str(data1) + " : " + str(data2))
+	except (Exception, psycopg.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.commit()
+			conn.close()
+
+def import_list(sqlstr, data, db_meta):
+	conn = None	
+	try:
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
+		cur = conn.cursor()
+		for item in data:
+			print(item)
+			sql = "INSERT INTO " + sqlstr + " VALUES (%s)"
+			data = (item,)
+			cur.execute(sql, data)
+	except (Exception, psycopg.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.commit()
+			conn.close()
+
+
+
+
+
+###
+
+def import_countries(data, db_meta):
+	conn = None
+	try:
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for item in data:
 			print(item['name'] + " : " + item['code'])
@@ -17,7 +82,7 @@ def import_countries(data,host):
 			country_code = item['code']
 			data = (country_name,country_code,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
@@ -26,51 +91,51 @@ def import_countries(data,host):
 
 ### Video
 
-def import_video_services(data,host):
+def import_video_services(data, db_meta):
 	conn = None	
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for item in data:
 			print(item)
 			sql = "INSERT INTO media_mediavideoservice(service_name) VALUES (%s)"
 			data = (item,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
 			conn.commit()
 			conn.close()
 
-def import_video_genres(data,host):
+def import_video_genres(data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for item in data:
 			print(item)
 			sql = "INSERT INTO media_mediavideogenre(genre) VALUES (%s)"
 			data = (item,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
 			conn.commit()
 			conn.close()
 
-def import_video_formats(data,host):
+def import_video_formats(data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for key, value in data.items():
 			print(key, value)
 			sql = "INSERT INTO media_mediavideoformat(doc_format,doc_format_name) VALUES (%s, %s)"
 			data = (key,value,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
@@ -80,34 +145,34 @@ def import_video_formats(data,host):
 
 ### Audio
 
-def import_audio_genres(data,host):
+def import_audio_genres(data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for item in data:
 			print(item)
 			sql = "INSERT INTO media_audiogenre(genre) VALUES (%s)"
 			data = (item,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
 			conn.commit()
 			conn.close()
 
-def import_audio_formats(data,host):
+def import_audio_formats(data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for key, value in data.items():
 			print(key, value)
 			sql = "INSERT INTO media_mediaaudioformat(doc_format,doc_format_name) VALUES (%s, %s)"
 			data = (key,value,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
@@ -117,17 +182,17 @@ def import_audio_formats(data,host):
 
 ### Photo
 
-def import_photo_formats(data,host):
+def import_photo_formats(data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for key, value in data.items():
 			print(key, value)
 			sql = "INSERT INTO media_mediaphotoformat(doc_format,doc_format_name) VALUES (%s, %s)"
 			data = (key,value,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
@@ -137,17 +202,17 @@ def import_photo_formats(data,host):
 
 ### Documents
 
-def import_doc_formats(data,host):
+def import_doc_formats(data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for key, value in data.items():
 			print(key, value)
 			sql = "INSERT INTO media_mediadocformat(doc_format,doc_format_name) VALUES (%s, %s)"
 			data = (key,value,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
@@ -155,17 +220,17 @@ def import_doc_formats(data,host):
 			conn.close()
 
 
-def import_services(data,host):
+def import_services(data, db_meta):
 	conn = None
 	try:
-		conn = psycopg2.connect(host=host, dbname="sgc", user="sgc", password="sgcmedia")
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
 		cur = conn.cursor()
 		for item in data:
 			print(item)
 			sql = "INSERT INTO media_mediaservice(service) VALUES (%s)"
 			data = (item,)
 			cur.execute(sql, data)
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, psycopg.DatabaseError) as error:
 		print(error)
 	finally:
 		if conn is not None:
@@ -173,22 +238,64 @@ def import_services(data,host):
 			conn.close()
 		
 
+### ROKU_CONTENT
+
+def import_languages(data, db_meta):
+	conn = None
+	try:
+		conn = psycopg.connect(host=db_meta['DB_HOST'], dbname=db_meta['DB_NAME'], user=db_meta['DB_USER'], password=db_meta['DB_PASSWORD'])
+		cur = conn.cursor()
+		for item in data:
+			print(item['code_iso_639_2'] + ", " + item['code_iso_639_1'] + ", " + item['language_name_eng'])
+			sql = "INSERT INTO roku_content_language(code_iso_639_2,code_iso_639_1,language_name_eng) VALUES (%s, %s, %s)"
+			lang1 = item['code_iso_639_2']
+			lang2 = item['code_iso_639_1']
+			lang3 = item['language_name_eng']
+			data = (lang1,lang2,lang3,)
+			cur.execute(sql, data)
+	except (Exception, psycopg.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.commit()
+			conn.close()
+
 
 
 if __name__ == "__main__":
 
 	print();print("Importing data...");print()
 
-	host="192.168.0.13"
+	from decouple import config
+	DB_HOST = config('DB_HOST')
+	DB_NAME = config('DB_NAME')
+	DB_USER = config('DB_USER')
+	DB_PASSWORD = config('DB_PASSWORD')
+	db_meta = {'DB_HOST':DB_HOST,'DB_NAME':DB_NAME,'DB_USER':DB_USER,'DB_PASSWORD':DB_PASSWORD}
 
 
-	
-	# Countries
-	f = open("country-list.json", "r")
+	# Languages
+	# print("Importing languages...")
+	# f = open("language-codes.json", "r")
+	# data = json.load(f)
+	# f.close()
+	# import_languages(data,db_meta)
+	# print()
+
+	# Import Genres
+	print("Importing content genres...")
+	f = open("content-genres.json", "r")
 	data = json.load(f)
 	f.close()
-	import_countries(data,host)
+	import_list("roku_content_genre(genre)", data, db_meta)
 	print()
+	
+	# Countries
+	# f = open("country-list.json", "r")
+	# data = json.load(f)
+	# f.close()
+	# import_countries(data,db_meta)
+	# print()
 
 	### Video
 
@@ -196,21 +303,21 @@ if __name__ == "__main__":
 	#f = open("video-services.json", "r")
 	#data = json.load(f)
 	#f.close()
-	#import_video_services(data,host)
+	#import_video_services(data,db_meta)
 	#print()
 	
 	# Video Genres
 	# f = open("video-genres.json", "r")
 	# data = json.load(f)
 	# f.close()
-	# import_video_genres(data,host)
+	# import_video_genres(data,db_meta)
 	# print()
 
 	#Video Formats
 	# f = open("video-formats.json", "r")
 	# data = json.load(f)
 	# f.close()
-	# import_video_formats(data,host)
+	# import_video_formats(data,db_meta)
 	# print()
 
 
@@ -219,14 +326,14 @@ if __name__ == "__main__":
 	# f = open("audio-genres.json", "r")
 	# data = json.load(f)
 	# f.close()
-	# import_audio_genres(data,host)
+	# import_audio_genres(data,db_meta)
 	# print()
 
 	#Audio Formats
 	# f = open("audio-formats.json", "r")
 	# data = json.load(f)
 	# f.close()
-	# import_audio_formats(data,host)
+	# import_audio_formats(data,db_meta)
 	# print()
 
 
@@ -235,7 +342,7 @@ if __name__ == "__main__":
 	# f = open("photo-formats.json", "r")
 	# data = json.load(f)
 	# f.close()
-	# import_photo_formats(data,host)
+	# import_photo_formats(data,db_meta)
 	# print()
 
 	### Documents
@@ -243,7 +350,7 @@ if __name__ == "__main__":
 	# f = open("doc-formats.json", "r")
 	# data = json.load(f)
 	# f.close()
-	# import_doc_formats(data,host)
+	# import_doc_formats(data,db_meta)
 	# print()
 
 
---
+++ imports/menu-import.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+# A menu driven shell script template 
+## ----------------------------------
+# Step #1: Define variables
+# ----------------------------------
+EDITOR=vim
+PASSWD=/etc/passwd
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+
+# ----------------------------------
+# Step #2: User defined function
+# ----------------------------------
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+
+one(){
+	echo "Importing all JSON data to Database..."
+	python3 imports.py all
+	pause
+}
+
+two(){
+	echo "Importing Countries..."
+	python3 imports.py country
+	pause
+}
+
+three(){
+	echo "Importing Video Formats..."
+	python3 imports.py vf
+	pause
+}
+
+four(){
+	echo "Importing Video Services..."
+	python3 imports.py vs
+	pause
+}
+
+five(){
+	echo "Importing Video Genres..."
+	python3 imports.py vg
+	pause
+}
+ 
+six(){
+	echo "Importing Audio Formats..."
+	python3 imports.py vf
+	pause
+}
+
+seven(){
+	echo "Importing Audio Services..."
+	python3 imports.py vs
+	pause
+}
+
+eight(){
+	echo "Importing Audio Genres..."
+	python3 imports.py vg
+	pause
+}
+
+nine(){
+	echo "Importing Photo Formats..."
+	python3 imports.py pf
+	pause
+}
+
+ten(){
+	echo "Importing Photo Services..."
+	python3 imports.py ps
+	pause
+}
+
+eleven(){
+	echo "Importing Document Formats..."
+	python3 imports.py df
+	pause
+}
+
+twelve(){
+	echo "Importing Languages..."
+	python3 imports.py lang
+	pause
+}
+
+thirteen(){
+	echo "Importing Countries..."
+	python3 imports.py cs
+	pause
+}
+
+fourteen(){
+	echo "Importing Content Genres..."
+	python3 imports.py cg
+	pause
+}
+
+fifteen(){
+	echo "Importing External IDs..."
+	python3 imports.py ei
+	pause
+}
+
+sixteen(){
+	echo "Importing Rating Sources..."
+	python3 imports.py rs
+	pause
+}
+
+seventeen(){
+	echo "Importing Parental Ratings..."
+	python3 imports.py pr
+	pause
+}
+
+eightteen(){
+	echo "Importing Ratings..."
+	python3 imports.py rating
+	pause
+}
+
+nineteen(){
+	echo "Importing Search Genres..."
+	python3 imports.py sg
+	pause
+}
+
+
+# function to display menus
+show_menus() {
+	clear
+	echo "----------------------------------"	
+	echo " SGC-MEDIA STATIC DATA IMPORTER"
+	echo " WARNING: Run any menu once only"
+	echo "----------------------------------"
+	echo
+	echo "1. All Data for Fresh Install"
+	echo
+	echo "Schema: MEDIA"
+	echo
+	echo "2. Countries"
+	echo "3. Video Formats"
+	echo "4. Video Services"
+	echo "5. Video Genres"
+	echo "6. Audio Formats"
+	echo "7. Audio Services"
+	echo "8. Audio Genres"
+	echo "9. Photo Formats"
+	echo "10. Photo Services"
+	echo "11. Document Formats"
+	echo
+	echo "Schema: ROKU_CONTENT"
+	echo	
+	echo "12. Language"
+	echo "13. Countries"
+	echo "14. Content Genres"
+	echo "15. External IDs"
+	echo "> Do these rating tables in order: 16, 17, 18"
+	echo "16. Rating Sources"
+	echo "17. Parental Ratings"
+	echo "18. Ratings"
+	echo
+	echo "Schema: ROKU_SEARCH"
+	echo
+	echo "19. Search Genres"
+	echo
+	echo "99. Exit"
+	echo
+}
+# read input from the keyboard and take a action
+# invoke the one() when the user select 1 from the menu option.
+# invoke the two() when the user select 2 from the menu option.
+# Exit when user the user select 3 form the menu option.
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 99] " choice
+	case $choice in
+		1) one ;;
+		2) two ;;
+		3) three ;;
+		4) four ;;
+		5) five ;;
+		6) six ;;
+		7) seven ;;
+		8) eight ;;
+		9) nine ;;
+		10) ten ;;
+		11) eleven ;;
+		12) twelve ;;
+		13) thirteen ;;
+		14) fourteen ;;
+		15) fifteen ;;
+		16) sixteen ;;
+		17) seventeen ;;
+		18) eighteen ;;
+		19) nineteen ;;
+		20) twenty ;;
+		99) exit 0;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+ 
+# ----------------------------------------------
+# Step #3: Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+trap '' SIGINT SIGQUIT SIGTSTP
+ 
+# -----------------------------------
+# Step #4: Main logic - infinite loop
+# ------------------------------------
+while true
+do
+	show_menus
+	read_options
+done
